e5f43447abb04ff720b89818f998e310840386d3
André Laszlo <andre@laszlo.nu>
2015-03-21 00:12:29 +0100

    Add border and list uploaded photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,5 @@
 class PhotosController < ApplicationController
+  def index
+    @photos = Photo.order(created_at: :desc).first(10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/mailgun/webhook', to: 'mailgun#webhook', via: :post
 
+  root 'photos#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
