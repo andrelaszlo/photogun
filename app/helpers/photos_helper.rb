560b5a7cbf7ed980b6e6868902417902b38c0ce6
André Laszlo <andre@laszlo.nu>
2015-03-22 15:39:40 +0100

    Show part of email address in photo viewer

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,9 @@
 module PhotosHelper
+  # Remove most of the local part of an email address
+  # user@example.com will be u...r@example.com
+  def redact_email(email)
+    name, domain = email.split '@'
+    redacted = "%s...%s" % [name[0], name[-1]]
+    "#{redacted}@#{domain}"
+  end
 end
