09072433260eb6d79145435223ee00131e7c04fe
André Laszlo <andre@laszlo.nu>
2015-03-22 17:17:07 +0100

    Fix resized gif bug

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,7 +4,7 @@ class Photo < ActiveRecord::Base
                     :styles => { :original => "1024x1024>", :thumb => "400x300#", :big => "1024x768>"},
                     :default_url => "/images/:style/missing.png",
                     :convert_options => {
-                      :big => ["-bordercolor '#{border_color}'", "-border 10x10"]
+                      :big => ["-coalesce", "-bordercolor '#{border_color}'", "-border 10x10"]
                     }
 
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
