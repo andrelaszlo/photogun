b2ce043e730b0e374c93682081fefd29a571d9fc
André Laszlo <andre@laszlo.nu>
2015-03-22 15:37:56 +0100

    Remove even row limitation in photo list

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,6 @@
 class PhotosController < ApplicationController
   def index
     @photos = Photo.order(created_at: :desc).first(99)
-    until @photos.length % 3 == 0
-      @photos.pop
-    end
-    @photos
   end
 
   def show
