a7c364f59858ebb7365b306bef1504ed4d96e07f
André Laszlo <andre@laszlo.nu>
2015-03-22 15:39:02 +0100

    Change border color to go with the rest of the design

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,10 @@
 class Photo < ActiveRecord::Base
+  border_color = "#27292B"
   has_attached_file :picture,
                     :styles => { :original => "1024x1024>", :thumb => "400x300#", :big => "1024x768>"},
                     :default_url => "/images/:style/missing.png",
                     :convert_options => {
-                      :big => ["-bordercolor black","-border 10x10"]
+                      :big => ["-bordercolor '#{border_color}'", "-border 10x10"]
                     }
 
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
